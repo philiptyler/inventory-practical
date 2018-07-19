df4275edd246faa00e560545a02b7579bdc47632
Philip Tyler <philip@theblacktux.com>
2018-07-19 11:10:20 -0700

    first commit files

---
+++ eng_practical.py
@@ -0,0 +1,45 @@
+import sys
+from inventory import Inventory
+
+inventory = {
+    'small': {
+        'slim': 5,
+        'reg': 5
+    },
+    'medium': {
+        'slim': 5,
+        'reg': 5
+    },
+    'large': {
+        'slim': 5,
+        'reg': 5
+    },
+}
+
+def format_input(request):
+    # this will throw a ValueError if there are not enough values
+    date, small, medium, large = request.split(',')
+    formatted_inputs = []
+    for input in request.split(','):
+        # this will throw a ValueError if the input cannot be cast
+        formatted_inputs.append(int(input))
+    return formatted_inputs
+
+event_requests = []
+for request in sys.argv[1:]:
+    try:
+        input = format_input(request)
+    except ValueError:
+        print ("Invalid input format. Please provide 4 integers in format: date,small,medium,large ie. 5,0,3,2")
+        exit(1)
+    else:
+        event_requests.append(input)
+
+inventory = Inventory()
+
+can_fufill = inventory.can_fufill_all_events(event_requests)
+if not can_fufill:
+    print("Buy More Tuxedos")
+    exit(1)
+
+print("Sufficient Inventory")
---
+++ inventory.py
@@ -0,0 +1,60 @@
+import copy
+
+SMALL = 'small'
+MEDIUM = 'medium'
+LARGE = 'large'
+SLIM = 'slim'
+REG = 'regular'
+
+class Inventory():
+    def __init__(self):
+        self.inventory = {
+            'slim': {'small': 5, 'medium': 5, 'large': 5},
+            'regular': {'small': 5, 'medium': 5, 'large': 5}
+        }
+
+    def can_fufill_all_events(self, events):
+        event_array = self.format_events(events)
+        for event in event_array:
+            can_fufill = self.can_fufill_event(event)
+            if not can_fufill:
+                return False
+        return True
+
+    def can_fufill_event(self, event):
+        new_inventory = copy.deepcopy(self.inventory)
+        can_fufill = True
+        # first try slim
+        for inven in new_inventory[SLIM]:
+            new_inventory[SLIM][inven] -= getattr(event, inven)
+            if new_inventory[SLIM][inven] < 1:
+                can_fufill = False
+                break;
+        if not can_fufill:
+            # now try regular
+            for inven in new_inventory[REG]:
+                new_inventory[REG][inven] -= getattr(event, inven)
+                if new_inventory[REG][inven] < 1:
+                    can_fufill = False
+                    break;
+        return can_fufill
+
+    def format_events(self, events):
+        event_array = []
+        for e in events:
+            new_event = Event(*e)
+            event_array.append(new_event)
+            event_array.append(new_event.get_return_event())
+        event_array.sort(key=lambda x: x.date)
+        return event_array
+
+
+class Event():
+    def __init__(self, date, small, med, large):
+        self.date = date
+        self.small = small
+        self.medium = med
+        self.large = large
+
+    def get_return_event(self):
+        return Event(self.date+3, -1 * self.small, -1 * self.medium, -1 * self.large)
